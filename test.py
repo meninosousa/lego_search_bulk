950dde10d912c93833591e3c7185c90cf935cf2e
meninosousa <meninosousa@gmail.com>
2016-08-20 23:49:32 +0200

    final no changes
    done

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ Created on Thu Jun 23 18:47:30 2016
 """
 
 import webbrowser
+import sys
 
 #url = 'http://www.python.org/'
 #
@@ -17,14 +18,41 @@ import webbrowser
 #webbrowser.open_new_tab(url + 'doc/')
 #
 #webbrowser.open_new(url)
-
 urls = ['https://www.fr.ricardo.ch/search/index/?SearchSentence=lego+','http://www.anibis.ch/fr/advertlist.aspx?fts=lego+']
 
+sets_rc = [4589,5599,5600,8366,8369,8376,8378,8475,8675,8676,'rc']
+sets_super = [853,956,8070,8145,8448,8466,8653,8860,8865,8880,42056,'super',5218]
+sets_control = [8485,8094,'control']
 
-#sets = [8366,5600,5599,8475,8376,8184,4589,8675,8183,8369,8378,8676,8448,8880,8466]
-#sets = [8448,8880,8466,5218]
-sets = [8485,8094]
 
-for each_set in sets:
-    for each_url in urls:
-        webbrowser.open_new_tab(each_url + str(each_set))
+
+def openner():
+    option = int(input('1_rc, 2_super, 2_control, 4_exit. pls choice: '))
+    sets = []
+
+    if option == 1:
+        sets = sets_rc
+
+    elif option == 2:
+        sets = sets_super
+
+    elif option == 3:
+        sets = sets_control
+
+    elif option == 4:
+        sys.exit()
+    else:
+        print('not an option')
+        openner()
+
+    for each_set in sets:
+         for each_url in urls:
+             webbrowser.open_new_tab(each_url + str(each_set))
+    openner()
+   
+def main():
+    openner()
+    
+    
+if __name__ == "__main__":
+    main()
